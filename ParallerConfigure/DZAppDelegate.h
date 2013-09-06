525e36751d4c80e66762090cb1dca974a6677191
dzpqzb <dz@wiz.cn>
2013-09-06 16:45:37 +0800

    Initial Commit

---
+++ ParallerConfigure/DZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DZAppDelegate.h
+//  ParallerConfigure
+//
+//  Created by dzpqzb on 13-9-6.
+//  Copyright (c) 2013年 dzpqzb inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ParallerConfigure/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParallerConfigure
+//
+//  Created by dzpqzb on 13-9-6.
+//  Copyright (c) 2013年 dzpqzb inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DZAppDelegate class]));
+    }
+}
